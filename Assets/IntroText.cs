6f11eb1aea64fb7cea9335f8ec20d281a849b4c9
MeganMackay <meg.souggie@ufl.edu>
2016-02-02 16:21:42 -0500

    upload scene in-progress

---
+++ Assets/IntroText.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class IntroText : MonoBehaviour {
+
+	public Text introText;
+	public Text trapText;
+	public Text ideaText;
+	public Text whatsThisText;
+	public Text exploreText;
+
+	public GameObject funnySkull;
+
+	void sayIntro(){
+		//funnySkull.SetActive(false);
+		trapText.text = "";
+		ideaText.text = "";
+		whatsThisText.text = "";
+		exploreText.text = "";
+		introText.text = "WE HAVE FINALLY ENTERED THE CAVE! LET'S FIND THE TREASURE!";
+	}
+
+	void sayTrapText(){
+		introText.text = "";
+		//ideaText.text = "";
+		trapText.text = "NO GOOD. WE NEED TO FIND SOME WAY TO GET THROUGH!";
+	}
+
+	void sayIdeaText(){
+		trapText.text = "";
+		ideaText.text = "MAYBE WE CAN FIND SOMETHING THIS WAY?";
+	}
+
+	void sayWhatsThis(){
+		ideaText.text = "";
+		whatsThisText.text = "IT SEEMS SOMETHING IS DOWN HERE!";
+	}
+
+	void sayExploreText(){
+		whatsThisText.text = "";
+		exploreText.text = "CAN YOU FIND THE HIDDEN TRIGGER?";
+	}
+
+	void explore(){
+		funnySkull.SetActive(true);
+		exploreText.text = "";
+	}
+}
